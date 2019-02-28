0ddf165a19b1eb7cc59541d38b86a724ddb550ad
restot <92ilya.icom@gmail.com>
2019-02-27 22:51:38 -0500

    Update frontend_helper_decorator.rb

--- app/helpers/spree/frontend_helper_decorator.rb
+++ app/helpers/spree/frontend_helper_decorator.rb
@@ -4,13 +4,10 @@ Spree::FrontendHelper.module_eval do
     return '' if root_taxon.leaf?
     taxons = root_taxon.children.map do |taxon|     
       desc = taxon.description.nil? || taxon.description.empty? ? '' : "<span class=\"taxonomies-subtaxon-description\">#{taxon.description}</span>"
-      if ENV['http_BADGES'].nil?
-        puts "http_BADGES #{ENV['http_BADGES'].inspect}"
-        badge = "<span class='badge badge-light'>#{Spree::Product.in_taxon(taxon).count}</span>"
-        link = link_to("<span class=\"taxonomies-subtaxon-name\">#{taxon.name}</span> #{badge}#{desc}".html_safe, seo_url(taxon), class: 'taxonomies-subtaxon-link')
-      else 
+        #badge = "<span class='badge badge-light'>#{Spree::Product.in_taxon(taxon).count}</span>"
+        #link = link_to("<span class=\"taxonomies-subtaxon-name\">#{taxon.name}</span> #{badge}#{desc}".html_safe, seo_url(taxon), class: 'taxonomies-subtaxon-link')
+  
         link = link_to("<span class=\"taxonomies-subtaxon-name\">#{taxon.name}</span>#{desc}".html_safe, seo_url(taxon), class: 'taxonomies-subtaxon-link')
-      end
       "<div class=\"taxonomies-subtaxon\">#{link}</div>"
     end
     return '' if taxons.empty? && !always
